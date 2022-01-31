9e3386a99601ba0535ba796005da1ab951f25289
Oscar Sykäri <oscar.sykari@aalto.fi>
2022-01-31 18:40:15 +0200

    add main.css

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 
 application = Flask(__name__)
+application.secret_key = "cockandballs"
 
-@application.route("/<name>")
-def index(name):
-    return "<h1>index {}</h1>".format(name)
 
+@application.route("/")
+def index():
+    flash("Player name")
+    return render_template("index.html")
+
+
+@application.route("/player", methods=["POST", "GET"])
+def player():
+    flash("It was " + str(request.form["name_input"]))
+    return render_template("index.html")
