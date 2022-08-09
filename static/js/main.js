39407f81126d1dd91a74226bea985b5f7673feb8
garga <osykari@gmail.com>
2022-08-09 17:27:16 +0300

    add reigns table
    Shows reign count for each player for currently visible filtering.

--- static/js/main.js
+++ static/js/main.js
@@ -4,15 +4,17 @@ var selectedMode = '435';
 var allTables = {};
 var godNames;
 var godRoles;
+var players;
 var modes = {426: "Conquest", 435: "Arena", 448: "Joust", 445: "Assault", 10195: "Under 30 Arena",
     451: "Conquest Ranked", 10193: "Under 30 Conquest", 10197: "Under 30 Joust", 450: "Joust Ranked",
     10189: "Slash", 440: "Duel Ranked"};
 var checkboxes;
 var roleSelectButton;
-function load(gods, roles, tables) {
+function load(gods, roles, tables, tableHeaders) {
     godNames = gods;
     godRoles = roles;
     allTables = tables;
+    players = tableHeaders;
     checkboxes = document.querySelectorAll('input[type="checkbox"]');
     checkboxes.forEach(checkbox => {
         checkbox.checked = true;
@@ -50,8 +52,8 @@ function selectAllRoles() {
     generateTable();
 }
 function generateTable() {
+    console.clear();
     if (selectedRoles.length == 0) {
-        console.log("should red");
         document.getElementById("role-select-button").style.color = '#dd0000';
     } else {
         document.getElementById("role-select-button").style.color = '#c9d1d9';
@@ -61,29 +63,42 @@ function generateTable() {
     tds = document.getElementsByClassName("data-column");
     trs = document.getElementsByClassName("data-row");
     values = allTables[selectedMode][selectedColumn].flat();
+    reigns = {}
+    players.forEach(player => reigns[player] = 0);
     for (let i = 0; i < tds.length; i++) {
         var row = Math.floor(i/3);
         if (!selectedRoles.includes(godRoles[godNames[row]])) {
             trs[row].style.display = "none";
         } else {
             trs[row].style.display = "table-row";
+            if (i % 3 == 2) {
+                biggest = i;
+    //            console.log(players[i%3-1]);
+                if (tds[i-1].innerHTML > tds[i].innerHTML) {
+                    biggest = i-1;
+                    if (tds[i-2].innerHTML > tds[i-1].innerHTML) {
+                        biggest = i-2;
+                    }
+                } else if (tds[i-2].innerHTML > tds[i].innerHTML) {
+                    biggest = i-2;
+                }
+                if (tds[biggest].innerHTML != 0) {
+                    for (j = 0; j < players.length; j++) {
+                        if (i - biggest == j) {
+                            name = players[(players.length - 1 - j)];
+                            reigns[name] = reigns[name] + 1;
+                        }
+                    }
+                    tds[biggest].style.fontWeight = "bold";
+                    tds[biggest].style.letterSpacing = "0.5px";
+                }
+            }
         }
         tds[i].innerHTML = values[i];
         tds[i].style.textDecoration = "none";
-
-        if (i % 3 == 2) {
-            biggest = tds[i];
-            if (tds[i-1].innerHTML > tds[i].innerHTML) {
-                biggest = tds[i-1];
-                if (tds[i-2].innerHTML > tds[i-1].innerHTML) {
-                    biggest = tds[i-2];
-                }
-            } else if (tds[i-2].innerHTML > tds[i].innerHTML) {
-                biggest = tds[i-2];
-            }
-            if (biggest.innerHTML != 0) {
-                biggest.style.textDecoration = "underline";
-            }
-        }
     }
+    players.forEach(player => {
+       document.getElementById(player + "-reigns").innerHTML = reigns[player];
+    });
+    console.log(reigns);
 }
